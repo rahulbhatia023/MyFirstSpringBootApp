a19bb2f9340dce6d1b8548bda7486541634ef0fb
Rahul Bhatia <rahul.bhatia023@gmail.com>
2017-12-12 01:10:09 +0530

    .

--- src/main/java/com/rahul/springboot/app/SpringBootApp.java
+++ src/main/java/com/rahul/springboot/app/SpringBootApp.java
@@ -1,13 +1,28 @@
 package com.rahul.springboot.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableSwagger2
-public class SpringBootApp {
+public class SpringBootApp implements CommandLineRunner {
+    @Autowired
+    private ApplicationContext applicationContext;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String[] beans = applicationContext.getBeanDefinitionNames();
+        Arrays.sort(beans);
+        Arrays.stream(beans).forEach(System.out::println);
     }
 }
